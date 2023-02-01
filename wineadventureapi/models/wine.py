fb3ffbe2f92e74ebed99dceefe5052813ac3d9eb
Tatiana Pronoza <tpronoza1@gmail.com>
2023-01-31 23:37:10 -0600

    reimported data and adjusted views for wine and wine101

--- wineadventureapi/models/wine.py
+++ wineadventureapi/models/wine.py
@@ -15,7 +15,6 @@ class Wine(models.Model):
     favorite = models.BooleanField(default=False)
     wish_list = models.BooleanField(default=False)
     wine_list = models.BooleanField(default=False)
-    uid = models.CharField(max_length=100)
     country_name = models.CharField(max_length=100)
     @property
     def winecategory(self):
--- wineadventureapi/models/wine101.py
+++ wineadventureapi/models/wine101.py
@@ -4,8 +4,8 @@ from .user import User
 
 class Wine101(models.Model):
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     article_name = models.CharField(max_length=50)
     context = models.CharField(max_length=100)
     article_image = models.CharField(max_length=100)
     article_link = models.CharField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
